e9135c35f1865254e06b423b2af60e8baf2a0b57
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-20 22:44:46 +0100

    Create operate function
    The operator function is designed to take input from the user and call the relevant
    operator function.

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ function divide(a,b) {
     return a / b;
 }
 
+// calls an operator function based on input
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case "+":
+            add(firstNumber, secondNumber);
+            break;
+        case "-":
+            subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            divide(firstNumber, secondNumber);
+            break;
+    }
+}
+
 let firstNumber;
 let secondNumber;
 let operator;
