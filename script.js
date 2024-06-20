d562cd422694889bdf90ecc54a9bc814ab6bbf60
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-20 22:48:20 +0100

    Change indentation to two spaces

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function add(a,b) {
-    return a + b;
+	return a + b;
 }
 
 function subtract(a,b) {
-    return a - b;
+	return a - b;
 }
 
 function multiply(a,b) {
-    return a * b;
+	return a * b;
 }
 
 function divide(a,b) {
-    return a / b;
+	return a / b;
 }
 
 // calls an operator function based on input
 function operate(operator, firstNumber, secondNumber) {
-    switch (operator) {
-        case "+":
-            add(firstNumber, secondNumber);
-            break;
-        case "-":
-            subtract(firstNumber, secondNumber);
-            break;
-        case "*":
-            multiply(firstNumber, secondNumber);
-            break;
-        case "/":
-            divide(firstNumber, secondNumber);
-            break;
-    }
+switch (operator) {
+	case "+":
+		add(firstNumber, secondNumber);
+		break;
+	case "-":
+		subtract(firstNumber, secondNumber);
+		break;
+	case "*":
+		multiply(firstNumber, secondNumber);
+		break;
+	case "/":
+		divide(firstNumber, secondNumber);
+		break;
+}
 }
 
 let firstNumber;
